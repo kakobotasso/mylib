efe368878e735640543ad81beac0e050ef5a5e91
Kako Botasso <kakodev@gmail.com>
2014-07-13 02:01:24 -0300

    Criando a parte de livros

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,10 @@
 class Book < ActiveRecord::Base
   belongs_to :author
+
+  validates :name, presence: true
+  validates :author_id, presence: true
+  validates :grade, presence: true
+  validates :wishlist, presence: true
+  validates :link, presence: true 
+  validates :read, inclusion: [true, false]
 end
