6edf7e95ed0b3e6c608e31458f7b841745e54094
Kako Botasso <kakodev@gmail.com>
2014-07-20 00:27:03 -0300

    Corrigindo validação da Lista de Desejos

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ActiveRecord::Base
   validates :name, presence: true
   validates :author_id, presence: true
   validates :grade, presence: true
-  validates :wishlist, presence: true
+  validates :wishlist, inclusion: [true, false]
   validates :link, presence: true 
   validates :read, inclusion: [true, false]
 end
