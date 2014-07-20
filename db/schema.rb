6389eb6eddac48e0a9502a2c998820f19c2f9e87
Kako Botasso <kakodev@gmail.com>
2014-07-20 12:49:20 -0300

    Adicionando permissão aos usuários

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140720144135) do
+ActiveRecord::Schema.define(version: 20140720153739) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20140720144135) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "permission", default: 1
   end
 
 end
